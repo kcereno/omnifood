305120c664a4bd5ad3e896adb3b4dfc6d849bdc6
Karl Cereno <kagc.dev@gmail.com>
2022-10-14 11:19:24 -0400

    Add sass map to spacing

--- src/sections/HeroSection/HeroSection.tsx
+++ src/sections/HeroSection/HeroSection.tsx
@@ -12,7 +12,7 @@ function HeroSection() {
           <h1 className="heading-primary mb-md">
             A healthy meal delivered to your door, every single day!
           </h1>
-          <p className="hero-description mb-lg ">
+          <p className="hero-description mb-lg">
             The smart 365-days-per-year food subscription that will make you eat
             healthy again. Tailored to your personal tastes and nutritional
             needs.
