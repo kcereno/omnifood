ba1470eaf536aafeb586c08d780d3b806370cf1a
Karl Cereno <kagc.dev@gmail.com>
2022-11-10 12:39:16 -0500

    Finish

--- src/sections/HeaderSection/HeaderSection.tsx
+++ src/sections/HeaderSection/HeaderSection.tsx
@@ -1,14 +1,47 @@
 import "./HeaderSection.scss";
 import logo from "../../assets/images/omnifood-logo.png";
-import Navbar from "../../components/UI/Navbar/Navbar";
+import { IoMenuSharp, IoCloseOutline } from "react-icons/io5";
 
 function HeaderSection() {
   return (
-    <header className="header d-flex justify-content-between align-items-center bg-tint">
+    <header className="header d-flex justify-content-between align-items-center bg-tint px-md nav-open">
       <a href="/">
         <img src={logo} className="header-logo" alt="logo" />
       </a>
-      <Navbar />
+      {/* Navbar */}
+      <nav className="navbar">
+        <ul className="navbar-list d-flex align-items-center flex-gap-sm">
+          <li>
+            <a className="navbar-link" href="/">
+              How It Works
+            </a>
+          </li>
+          <li>
+            <a className="navbar-link" href="/">
+              Meals
+            </a>
+          </li>
+          <li>
+            <a className="navbar-link" href="/">
+              Testimonials
+            </a>
+          </li>
+          <li>
+            <a className="navbar-link" href="/">
+              Pricing
+            </a>
+          </li>
+          <li>
+            <a className="navbar-link navbar-cta bg-primary" href="/">
+              Try for free
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <button className="btn-mobile-nav">
+        <IoMenuSharp className="icon-mobile-nav" name="menu-outline" />
+        <IoCloseOutline className="icon-mobile-nav" name="close-outline" />
+      </button>
     </header>
   );
 }
