3d305af4406a368b73de8025092d780e765fd6f4
Karl Cereno <kagc.dev@gmail.com>
2022-11-11 12:47:22 -0500

    Finish 135

--- src/sections/HeaderSection/HeaderSection.tsx
+++ src/sections/HeaderSection/HeaderSection.tsx
@@ -1,46 +1,69 @@
 import "./HeaderSection.scss";
 import logo from "../../assets/images/omnifood-logo.png";
 import { IoMenuSharp, IoCloseOutline } from "react-icons/io5";
+import { useState } from "react";
 
 function HeaderSection() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  let menuStatus = showMenu ? "nav-open" : null;
+
+  const handleOpenMenu = () => {
+    setShowMenu(true);
+  };
+
+  const handleCloseMenu = () => {
+    setShowMenu(false);
+  };
+
   return (
-    <header className="header d-flex justify-content-between align-items-center bg-tint px-md nav-open">
+    <header
+      className={`header d-flex justify-content-between align-items-center bg-tint px-md  ${menuStatus}`}
+    >
       <a href="/">
         <img src={logo} className="header-logo" alt="logo" />
       </a>
       {/* Navbar */}
-      <nav className="navbar">
-        <ul className="navbar-list d-flex align-items-center flex-gap-sm">
+      <nav className="main-nav">
+        <ul className="main-nav-list d-flex align-items-center flex-gap-sm">
           <li>
-            <a className="navbar-link" href="/">
+            <a className="main-nav-link" href="/">
               How It Works
             </a>
           </li>
           <li>
-            <a className="navbar-link" href="/">
+            <a className="main-nav-link" href="/">
               Meals
             </a>
           </li>
           <li>
-            <a className="navbar-link" href="/">
+            <a className="main-nav-link" href="/">
               Testimonials
             </a>
           </li>
           <li>
-            <a className="navbar-link" href="/">
+            <a className="main-nav-link" href="/">
               Pricing
             </a>
           </li>
           <li>
-            <a className="navbar-link navbar-cta bg-primary" href="/">
+            <a className="main-nav-link main-nav-cta bg-primary" href="/">
               Try for free
             </a>
           </li>
         </ul>
       </nav>
       <button className="btn-mobile-nav">
-        <IoMenuSharp className="icon-mobile-nav" name="menu-outline" />
-        <IoCloseOutline className="icon-mobile-nav" name="close-outline" />
+        <IoMenuSharp
+          className="icon-mobile-nav"
+          name="menu-outline"
+          onClick={handleOpenMenu}
+        />
+        <IoCloseOutline
+          className="icon-mobile-nav"
+          name="close-outline"
+          onClick={handleCloseMenu}
+        />
       </button>
     </header>
   );
